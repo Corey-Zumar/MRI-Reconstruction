4dabeaa9fd9fda86c8070a9301d9bba2b95aa114
Corey Zumar <czumar@berkeley.edu>
2017-10-30 22:34:22 -0700

    Fixes

--- eval_net.py
+++ eval_net.py
@@ -12,7 +12,7 @@ from matplotlib import pyplot as plt
 def load_image(image_path):
     original_img = nib.load(image_path)
     original_data = np.squeeze(original_img.get_data())
-    original_data = np.moveaxis(original_data, -1, 0).reshap(128, 256, 256, 1)
+    original_data = np.moveaxis(original_data, -1, 0).reshape(128, 256, 256, 1)
 
     subsampled_img = Subsample.subsample(image_path)
     subsampled_data = np.moveaxis(subsampled_img, -1, 0).reshape(128, 256, 256, 1)
@@ -31,10 +31,10 @@ def main():
     test_subsampled, test_original = load_image(args.img_path)
     fnet = load_net(args.net_path)
 
-    result = fnet.predict(test_subsampled[0].reshape(1, 256, 256, 1))
+    result = fnet.predict(test_subsampled[70].reshape(1, 256, 256, 1))
 
     magnitude_spectrum = 20*np.log(np.abs(result.reshape(256,256)))
-    plt.subplot(121),plt.imshow(test_original, cmap = 'gray')
+    plt.subplot(121),plt.imshow(test_original[70].reshape(256,256), cmap = 'gray')
     plt.title('Input Image'), plt.xticks([]), plt.yticks([])
     plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
     plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
--- train_net.py
+++ train_net.py
@@ -136,7 +136,7 @@ class FNet:
 def load_image(image_path):
 	img = nib.load(image_path)
 	data = img.get_data()
-	return data
+	return np.squeeze(data)
 
 def load_and_subsample_images(disk_path):
 	"""
