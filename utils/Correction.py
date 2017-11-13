1a95892cd4518d31533ee84824b484dcfb4bb867
Alex-Kot <32029447+Alex-Kot@users.noreply.github.com>
2017-11-12 22:42:36 -0800

    Update Correction.py

--- utils/Correction.py
+++ utils/Correction.py
@@ -10,7 +10,7 @@ import numpy as np
 import nibabel as nib
 from matplotlib import pyplot as plt
 
-def subsample(subsampled_img, network_output, substep, lowfreqPercent):
+def Correction(subsampled_img, network_output, substep, lowfreqPercent):
     """
     Corrects network output using the input subsampled image.
 
@@ -32,38 +32,39 @@ def subsample(subsampled_img, network_output, substep, lowfreqPercent):
     interpretable images.
     """
 
-    for slice in range(network_output.shape[2]):
-        network_output_slice = np.squeeze(network_output[:,:,slice])
-        subsampled_slice = np.squeeze(subsampled_slice[:,:,slice])
-
         # 2-dimensional fast Fourier transform
-        t_output = np.fft.fft2(network_output_slice)
-        t_input = np.fft.fft2(subsampled_slice)
+    t_output = np.fft.fft2(network_output)#_slice)
+    t_input = np.fft.fft2(subsampled_img)#_slice)
 
         # shifts 0 frequency to center
-        tshift_output = np.fft.fftshift(t_output)
-        tshift_input = np.fft.fftshift(t_input)
+    tshift_output = np.fft.fftshift(t_output)
+    tshift_input = np.fft.fftshift(t_input)
+
+    mo = 20*np.log(np.abs(tshift_output.reshape(256,256)))
+    mi = 20*np.log(np.abs(tshift_input.reshape(256,256)))
+    plt.subplot(121),plt.imshow(mo, cmap = 'gray', vmin=0, vmax=255)
+    plt.title('Ground_Truth'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122),plt.imshow(mi, cmap = 'gray',vmin=0, vmax=255)
+    plt.title('After_Correction'), plt.xticks([]), plt.yticks([])
+    plt.show()
+
 
         #Subsampler,
         #accounts for the double-counted lines
-        lowfreqModifiedPercent = 1.0/float(substep)*lowfreqPercent+lowfreqPercent
+    lowfreqModifiedPercent = 1.0/float(substep)*lowfreqPercent+lowfreqPercent
 
-        start = len(tshift)/2-int(lowfreqModifiedPercent*float(len(tshift)))
-        end = len(tshift)/2+int(lowfreqModifiedPercent*float(len(tshift)))
+    start = len(tshift_output)/2-int(lowfreqModifiedPercent*float(len(tshift_output)))
+    end = len(tshift_output)/2+int(lowfreqModifiedPercent*float(len(tshift_output)))
 
-        for i in range(0, start):
-            if i % substep == 0:
-                tshift_output[i] = tshift_input[i]
-        for i in range (start, end):
+    for i in range(0, start):
+        if i % substep == 0:
             tshift_output[i] = tshift_input[i]
-        for i in range (end, len(tshift)):
-            if i % substep == 0:
-                tshift_output[i] = tshift_input[i]
+    for i in range (start, end):
+        tshift_output[i] = tshift_input[i]
+    for i in range (end, len(tshift_output)):
+        if i % substep == 0:
+            tshift_output[i] = tshift_input[i]
 
         # Visualize result of subsample #
-        corr = abs(np.fft.ifft2(tshift_output))
-        imgarr[:,:,slice,0] = corr
-        print(slice)
-        print(type(imgarr))
-
-    return np.squeeze(imgarr)
+    corr = abs(np.fft.ifft2(np.fft.ifftshift(tshift_output)))
+    return corr
