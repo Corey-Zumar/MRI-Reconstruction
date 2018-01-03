72fd3d4aa633618149331e55e25484dd3fdf8187
Corey Zumar <czumar@berkeley.edu>
2018-01-02 18:43:59 -0800

    Pip package support

---
+++ submrine/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import os
+
+with open('LICENSE.txt', "r") as f:
+    license = f.read()
+
+with open("VERSION.txt", "r") as fp:
+    version = fp.read().strip()
+
+setup(
+    name='submrine',
+    version=version,
+    description='',
+    maintainer='Corey Zumar, Alex Kot',
+    maintainer_email='czumar@berkeley.edu, akot@berkeley.edu',
+    license=license,
+    packages=[
+        "submrine", "submrine.utils", "submrine.train", "submrine.eval"
+    ],
+    package_data={'submrine': ['*.txt']},
+    keywords=['submrine', 'mri', 'reconstruction', 'deep learning'],
+    install_requires=[
+        'numpy>=1.13.1',
+        'nibabel>=2.2.0',
+        'matplotlib>=2.0.2',
+        'keras>=2.0.6',
+        'scikit-image>=0.13.1'
+    ])
